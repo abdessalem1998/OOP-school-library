c5a65d76554152308cf188f2be8a54114f2d2721
Houfaf abdessalem <abdeslam.dib.usa@gmail.com>
2022-03-19 00:09:57 +0100

    define needed funtcions

--- app.rb
+++ app.rb
@@ -1,5 +1,39 @@
 class App
+  def initialize
+    @people = []
+    @books = []
+    @rentals = []
+  end
+
+  def list_books
+    puts 'list_books'
+  end
+
+  def list_people
+    puts 'list_people'
+  end
+
+  def create_person
+    puts 'create_person'
+  end
+
+  def create_book
+    puts 'create_person'
+  end
+
+  def create_rental
+    puts 'create_person'
+  end
+
+  def list_rentals
+    puts 'list_rentals'
+  end
+
+  def start
+    puts 'start'
+  end
 
 end
 
 app = App.new
+app.start
