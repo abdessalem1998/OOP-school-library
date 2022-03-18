feaa876fbe7e21bad850e8df0142a5da26d77593
Houfaf abdessalem <abdeslam.dib.usa@gmail.com>
2022-03-19 00:26:26 +0100

    Logic code to interact with user

--- app.rb
+++ app.rb
@@ -29,8 +29,49 @@ class App
     puts 'list_rentals'
   end
 
+  def print_start_message
+    puts 'Please choose an option:'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person'
+    puts '7 - Exit'
+  end
+
   def start
-    puts 'start'
+    puts "Welcome to our school library app!"
+    loop do
+        puts "Please choose an option:"
+        puts "1 - List all books"
+        puts "2 - List all people"
+        puts "3 - Create a person"
+        puts "4 - Create a book"
+        puts "5 - Create a rental"
+        puts "6 - List all rentals for a given person"
+        puts "7 - Exit"
+
+    option = gets.chomp.to_i
+
+    case option
+    when 1
+      list_books
+    when 2
+      list_people
+    when 3
+      create_person
+    when 4
+      create_book
+    when 5
+      create_rental
+    when 6
+      list_rentals
+    when 7
+      puts "Okay. Bye"
+      break
+    end
+    end
   end
 
 end
