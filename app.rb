83bbe27ef1da30fbb67bb3019e49112663c93f0f
Houfaf abdessalem <abdeslam.dib.usa@gmail.com>
2022-03-19 02:14:36 +0100

    Fix linters

--- app.rb
+++ app.rb
@@ -1,7 +1,8 @@
-require './student.rb'
-require './teacher.rb'
-require './book.rb'
-require './rental.rb'
+require './helper'
+require './student'
+require './teacher'
+require './book'
+require './rental'
 
 class App
   def initialize
@@ -11,41 +12,35 @@ class App
   end
 
   def list_books
-    puts "No books found" if @books.empty?
-    puts(@books.map {|book| "Title #{book.title}, Author: #{book.author}" })
+    puts 'No books found' if @books.empty?
+    puts(@books.map { |book| "Title #{book.title}, Author: #{book.author}" })
   end
 
   def list_people
-    puts "No people found" if @people.empty?
-    puts(@people.map {|person| "#{person.class.name} Name: #{person.name}, Age: #{person.age}" })
+    puts 'No people found' if @people.empty?
+    puts(@people.map { |person| "#{person.class.name} Name: #{person.name}, Age: #{person.age}, ID: #{person.id}" })
   end
 
   def create_person
-    print('Do you want to create a new student (1) or a new teacher? (2) [1/2]: ')
-    option = gets.chomp.to_i
+    option = get_promt('Do you want to create a new student (1) or a new teacher? (2) [1/2]: ').to_i
 
     unless [1, 2].include?(option)
-      puts "Invalid selection"
+      puts 'Invalid selection'
       return
     end
 
-    print('Age: ')
-    age = gets.chomp.to_i
-    print('Name: ')
-    name = gets.chomp
+    age = get_promt('Age: ')
+    name = get_promt('Name: ')
 
     case option
     when 1
-      print('Has parent permission? [y/n]: ')
-      permission = gets.chomp === 'y' ? true : false
-      student = Student.new(name, 'class', age, permission)
+      permission = get_promt('Has parent permission? [y/n]:') == 'y'
+      student = Student.new(name, 'class', age, parent_permission: permission)
       @people << student
     when 2
-      print('Specialization: ')
-      specialization = gets.chomp
+      specialization = get_promt('Specialization')
       teacher = Teacher.new(age, specialization, name)
       @people << teacher
-
     end
     puts "Person #{name} created successfully!"
   end
@@ -58,33 +53,45 @@ class App
     book = Book.new(title, author)
 
     @books << book
-    puts "Book created successfully!"
+    puts 'Book created successfully!'
   end
 
   def create_rental
-    puts "Select a book from the following list by its number:"
-    puts (@books.each_with_index.map {|book, index| "#{index + 1}) Title: #{book.title} Author: #{book.author}"})
+    puts 'Select a book from the following list by its number:'
+    puts(@books.each_with_index.map { |book, index| "#{index + 1}) Title: #{book.title} Author: #{book.author}" })
     book_index = gets.chomp.to_i
 
-    puts "Select a person from the following list by its number:"
-    puts (@people.each_with_index.map {|person, index| "#{index + 1}) #{person.class.name} name: #{person.name} ID: #{person.id} age: #{person.age}"})
+    puts 'Select a person from the following list by its number:'
+    puts(@people.each_with_index.map do |person, index|
+           "#{index + 1}) #{person.class.name} name: #{person.name} ID: #{person.id} age: #{person.age}"
+         end)
     person_index = gets.chomp.to_i
 
-    print "Date [YYYY/MM/DD]: "
+    print 'Date [YYYY/MM/DD]: '
     date = gets.chomp
 
     rental = Rental.new(date, @books[book_index - 1], @people[person_index - 1])
 
     @rentals << rental
-    puts "Rental created successfully!"
-
+    puts 'Rental created successfully!'
   end
 
   def list_rentals
     print('Id of the person: ')
     id = gets.chomp.to_i
-    person = @people.filter {|person| person.id == id}.first
+    person = @people.filter { |p| p.id == id }.first
     puts(person.rentals.map { |rental| "Date: #{rental.date}, Book: #{rental.book.title}, by #{rental.book.author}" })
+  end
+
+  def print_start_message
+    puts 'Please choose an option:'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person'
+    puts '7 - Exit'
   end
 
   def execute(option)
@@ -104,27 +111,19 @@ class App
     end
   end
 
-
   def start
-    puts "Welcome to our school library app!"
+    puts 'Welcome to our school library app!'
     loop do
-        puts "Please choose an option:"
-        puts "1 - List all books"
-        puts "2 - List all people"
-        puts "3 - Create a person"
-        puts "4 - Create a book"
-        puts "5 - Create a rental"
-        puts "6 - List all rentals for a given person"
-        puts "7 - Exit"
+      print_start_message
 
-    option = gets.chomp.to_i
+      option = gets.chomp.to_i
 
-    if option == 7
-      puts 'Okay. Bye'
-      break
-    end
+      if option == 7
+        puts 'thanks!!'
+        break
+      end
 
-    execute(option)
+      execute(option)
     end
   end
 end
--- main.rb
+++ main.rb
@@ -1,4 +1,4 @@
-require './app.rb'
+require './app'
 
 def main
   app = App.new
