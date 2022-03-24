af2cb6ec7bf4efd85f067b755761d62a4a52a712
Houfaf abdessalem <abdeslam.dib.usa@gmail.com>
2022-03-24 22:27:59 +0100

    Test the book class

---
+++ spec/book_spec.rb
@@ -0,0 +1,17 @@
+require_relative '../book'
+
+describe Book do
+  context "Check if the instance is a class of Book" do
+
+    book = Book.new('title', 'author')
+
+    it "should have a class of Book" do
+      expect(book.instance_of?(Book)) == true
+    end
+
+    it "should have the right title and author" do
+      expect(book.title).to eq "title"
+      expect(book.author).to eq "author"
+    end
+  end
+end
